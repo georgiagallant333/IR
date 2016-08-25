0f25f740128282db56690c06bc2bf6c854f0796d
Georgia Gallant <georgia.gallant@gmail.com>
2016-08-24 21:31:26 -0400

    gets average of middle readings

--- MultipleSensorsWithTiming/MultipleSensorsWithTiming.ino
+++ MultipleSensorsWithTiming/MultipleSensorsWithTiming.ino
@@ -1,4 +1,4 @@
-#define NUMSAMPLES 20 
+#define NUMSAMPLES 20
 #define NUMSENSORS 7
 
 int incrementSamples[7];
@@ -23,13 +23,15 @@ void loop() {
   readSensors();
   if (endLoop == 1) {
     resetVariables();
-    sortArray();
+    sortArrayAndGetAverage();
     printSensors();
   }
 }
 void printSensors() {
   Serial.println("\nNEW SERIES");
-  for (int a = 0; a < NUMSAMPLES-2; a++) {
+  for (int a = 0; a < NUMSAMPLES; a++) {
+    Serial.print(a);
+    Serial.print(": ");
     for (int b = 0; b < NUMSENSORS; b++) {
       Serial.print(b);
       Serial.print(": ");
@@ -40,22 +42,34 @@ void printSensors() {
   }
 }
 
-void sortArray(){
-  for(int i = 0; i < NUMSENSORS; i++){
+void getAverage(long arr[]) {
+  long sum = 0;
+  long avg = 0;
+  for(int i = ((NUMSAMPLES / 2)-5); i < ((NUMSAMPLES / 2)+5); i++){
+    sum = sum + arr[i];
+  }
+  avg = (float)sum/10;
+  Serial.print("AVERAGE: ");
+  Serial.print(avg);
+}
+
+void sortArrayAndGetAverage() {
+  for (int i = 0; i < NUMSENSORS; i++) {
     insertion_sort(sensorTimesHigh[i], NUMSAMPLES);
+    getAverage(sensorTimesHigh[i]);
   }
 }
 
-void insertion_sort (long arr[], int length){
-    int j, temp;
-  for (int j = 1; j < length; j++){
-    while (j > 0 && arr[j] < arr[j-1]){
-        temp = arr[j];
-        arr[j] = arr[j-1];
-        arr[j-1] = temp;
-        j--;
-        }
+void insertion_sort (long arr[], int length) {
+  int j, temp;
+  for (int j = 1; j < length; j++) {
+    while (j > 0 && arr[j] < arr[j - 1]) {
+      temp = arr[j];
+      arr[j] = arr[j - 1];
+      arr[j - 1] = temp;
+      j--;
     }
+  }
 }
 
 
