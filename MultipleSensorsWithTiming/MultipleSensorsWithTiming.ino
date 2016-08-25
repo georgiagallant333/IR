76d933b4a2d720cf939d204d7be81da2044d2910
Georgia Gallant <georgia.gallant@gmail.com>
2016-08-24 20:40:42 -0400

    doesn't print first and last sensor readings

--- MultipleSensorsWithTiming/MultipleSensorsWithTiming.ino
+++ MultipleSensorsWithTiming/MultipleSensorsWithTiming.ino
@@ -1,49 +1,74 @@
-#define NUMSAMPLES 20
+#define NUMSAMPLES 20 
 #define NUMSENSORS 7
 
-int incrementSamples[NUMSAMPLES];
+int incrementSamples[7];
 long startTime[7];
-long finalTime[7];
 byte currentReading[7];
 byte pastReading[7];
+boolean endLoop = 0;
 
 long sensorTimesHigh[NUMSENSORS][NUMSAMPLES];
 
+
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   pinMode(13, OUTPUT);
-  for (int x = 2; x < 9; x++) {
+  //initializing pins
+  for (int x = 2; x < NUMSENSORS + 2; x++) {
     pinMode(x, INPUT);
   }
-
 }
 
 void loop() {
-  for (int x = 2; x < 9; x++) {
-    currentReading[x - 2] = digitalReadFast(x);
+  readSensors();
+  if (endLoop == 1) {
+    resetVariables();
+    printSensors();
   }
-  for (int x = 0; x < 7; x++) {
-    if (pastReading[x] == 1 && currentReading[x] == 0) {
-      startTime[x] = micros();
+}
+void printSensors() {
+  Serial.println("\nNEW SERIES");
+  for (int a = 1; a < NUMSAMPLES - 1; a++) {
+    for (int b = 0; b < NUMSENSORS; b++) {
+      Serial.print(b);
+      Serial.print(": ");
+      Serial.print(sensorTimesHigh[b][a]);
+      Serial.print(" ");
     }
-    else if (pastReading[x] == 0 && currentReading[x] == 1) {
-      finalTime[x] = micros() - startTime[x];
-      sensorTimesHigh[x][incrementSamples[x]] = finalTime[x];
-      incrementSamples[x]++;
-    }
-    pastReading[x] = currentReading[x];
-    if (incrementSamples[x] == NUMSAMPLES) {
-      incrementSamples[x] = 0;
-      for (int a = 0; a < NUMSAMPLES; a++) {
-        for (int b = 0; b < 7; b++) {
-          Serial.print(b);
-          Serial.print(": ");
-          Serial.println(sensorTimesHigh[b][a]);
-        }
-      }
-    }
+    Serial.println(" ");
   }
 }
 
 
+void resetVariables() {
+  endLoop = 0;
+  for (int i = 0; i < NUMSENSORS; i++) {
+    incrementSamples[i] = 0;
+  }
+  for (int i = 0; i < NUMSENSORS; i++) {
+    startTime[i] = 0;
+    pastReading[i] = 0;
+  }
+}
+
+void readSensors() {
+  if (endLoop == 0) {
+    for (int x = 2; x < NUMSENSORS + 2; x++) {
+      currentReading[x - 2] = digitalReadFast(x);
+    }
+    for (int x = 0; x < NUMSENSORS; x++) {
+      if (pastReading[x] == 1 && currentReading[x] == 0) {
+        startTime[x] = micros();
+      }
+      else if (pastReading[x] == 0 && currentReading[x] == 1) {
+        sensorTimesHigh[x][incrementSamples[x]] = micros() - startTime[x];
+        incrementSamples[x]++;
+      }
+      if (incrementSamples[x] == NUMSAMPLES) {
+        endLoop = 1;
+      }
+      pastReading[x] = currentReading[x];
+    }
+  }
+}
 
